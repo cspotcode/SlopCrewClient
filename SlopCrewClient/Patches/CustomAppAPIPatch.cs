a7b7937bdba4fe57e9bc74dd5a412d66f8e79759
Andrew Bradley <cspotcode@gmail.com>
2024-02-17 22:04:17 -0500

    Fix bug in CustomAppAPI by patching it, because I doubt it's every gonna be updated

---
+++ SlopCrewClient/Patches/CustomAppAPIPatch.cs
@@ -0,0 +1,37 @@
+﻿using System.Reflection;
+using HarmonyLib;
+using UnityEngine;
+
+namespace cspotcode.SlopCrewClient.Patches;
+[HarmonyPatch()]
+internal class CustomAppAPIPatch {
+    internal static void AttemptPatch() {
+        // Patch conditionally using reflection so that we don't have to link against it
+        var assembly = AppDomain.CurrentDomain.GetAssemblies().Where(a => a.GetName().Name == "CustomAppAPI").FirstOrDefault();
+        if (assembly == null) return;
+        var CustomAppModType = assembly.GetTypes().Where(t => t.Name == "CustomAppMod").FirstOrDefault();
+        if (CustomAppModType == null) return;
+        var original = CustomAppModType.GetMethod("FindDerivedTypes", BindingFlags.Static | BindingFlags.NonPublic);
+        if (original == null) return;
+        var prefix = typeof(CustomAppAPIPatch).GetMethod(nameof(FindDerivedTypes_Prefix), BindingFlags.Static | BindingFlags.NonPublic);
+    
+        var harmony = new Harmony(PluginInfo.PLUGIN_NAME);
+        harmony.Patch(original, new HarmonyMethod(prefix));
+    }
+    
+    // Target being patched:
+    // CustomAppAPI.CustomAppMod {
+    //   private static IEnumerable<Type> FindDerivedTypes(Assembly assembly, Type baseType) {
+    //     return assembly.GetTypes().Where(t => baseType.IsAssignableFrom(t) && t != baseType);
+    private static bool FindDerivedTypes_Prefix(Assembly assembly, Type baseType, ref IEnumerable<Type> __result) {
+        __result = assembly.GetTypes().Where(t => {
+            try {
+                return baseType.IsAssignableFrom(t) && t != baseType;
+            } catch(TypeLoadException e) {
+                // Swallow it
+                return false;
+            }
+        });
+        return false;
+    }
+}
---
+++ SlopCrewClient/Plugin.cs
@@ -0,0 +1,24 @@
+﻿using BepInEx;
+using cspotcode.SlopCrewClient.Patches;
+using UnityEngine;
+
+namespace cspotcode.SlopCrewClient;
+
+[BepInPlugin(PluginInfo.PLUGIN_GUID, PluginInfo.PLUGIN_NAME, PluginInfo.PLUGIN_VERSION)]
+public class Plugin : BaseUnityPlugin {
+    public static Plugin Instance;
+
+    public Plugin() {
+        this.Logger.LogInfo($"{Info.Metadata.Name} plugin loaded");
+    }
+
+    private void Awake() {
+        Instance = this;
+        CustomAppAPIPatch.AttemptPatch();
+    }
+    private void Update() {
+        // var installed = SlopCrewAPI.APIManager.IsSlopCrewInstalled;
+        // var connected = installed ? SlopCrewAPI.APIManager.API.Connected : false;
+        // Debug.Log($"installed:{installed} connected:{connected}");
+    }
+}
